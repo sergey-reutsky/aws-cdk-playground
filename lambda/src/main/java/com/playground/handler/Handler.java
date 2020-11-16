244f0278908e7fc23051f8dd18ccd00de54a027d
Sergey Reutsky <sergey.reutsky@gmail.com>
2020-11-16 12:50:12 +0300

    change lambda's input param type

--- lambda/src/main/java/com/playground/handler/Handler.java
+++ lambda/src/main/java/com/playground/handler/Handler.java
@@ -4,11 +4,11 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class Handler implements RequestHandler<String, String>
+public class Handler implements RequestHandler<Object, String>
 {
 
 	@Override
-	public String handleRequest(String event, Context context)
+	public String handleRequest(Object event, Context context)
 	{
 		LambdaLogger logger = context.getLogger();
 		String response = "200 OK";
