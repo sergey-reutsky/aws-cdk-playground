288d875725765f22cc2b7ac8ae95043f2087a694
Sergey Reutsky <sergey.reutsky@gmail.com>
2020-11-20 01:39:41 +0300

    update lambda handler

--- lambda/src/main/java/com/playground/handler/Handler.java
+++ lambda/src/main/java/com/playground/handler/Handler.java
@@ -3,22 +3,30 @@ package com.playground.handler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
-public class Handler implements RequestHandler<Object, String>
+import java.util.HashMap;
+import java.util.Map;
+
+public class Handler implements RequestHandler<Map<String,String>, String>
 {
 
+	final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
 	@Override
-	public String handleRequest(Object event, Context context)
+	public String handleRequest(Map<String,String> event, Context context)
 	{
-		LambdaLogger logger = context.getLogger();
-		String response = "200 OK";
-		// log execution details
-		logger.log("ENVIRONMENT VARIABLES: " + System.getenv());
-		logger.log("CONTEXT: " + context);
+		final LambdaLogger logger = context.getLogger();
+
 		// process event
-		logger.log("EVENT: " + event);
+		logger.log("EVENT: " + gson.toJson(event));
 		logger.log("EVENT TYPE: " + event.getClass().toString());
 
-		return response;
+		Map<String, Object> response = new HashMap<>();
+		response.put("statusCode", "200");
+		response.put("isBase64Encoded", false);
+
+		return gson.toJson(response);
 	}
 }
